7ffd7a05567b5a63873f7a3df351167fa71f6488
sernikb <sernikb@gmail.com>
2018-04-01 19:24:16 +0200

    TextSurface rendering optimalisation
     finnaly 10fps on win vista craptop!
    Every surface is put inside table and when detected its the same surface its used again and not rendered every frame

--- Paperbag/gui.py
+++ Paperbag/gui.py
@@ -5,6 +5,8 @@ import pygame
 import graph
 import os
 
+SpriteLoadedText = []
+
 class GUIMainMenuButton:
 
     IsGUI = True
@@ -23,8 +25,33 @@ class GUIMainMenuButton:
 #basic text object
 def text_object( Surface, Size, Text, Font, Color, POS ):
         #                                              V String Escape! 
-        Font = pygame.font.Font(os.path.join("content\\fonts", Font ), Size)
-        NewSurface = Font.render(Text, False, Color, None)
-        NewSurfaceShadow = Font.render(Text, False, (0,0,0), None)
-        Surface.blit(NewSurfaceShadow,(POS[0]-5,POS[1]-5))
-        Surface.blit(NewSurface,POS)
+        FontObj = pygame.font.Font(os.path.join("content\\fonts", Font ), Size)
+
+        LoadedSurface = False
+        LoadedSurfaceShadow = False
+        for K in SpriteLoadedText:
+                if K[0] == Text+"&"+Font+"&"+str(Size):
+                        LoadedSurface = K[1]
+                if K[0] == Text+"&"+Font+"&"+str(Size)+"&shadow":
+                        LoadedSurfaceShadow = K[1]
+                        
+        if LoadedSurface != False:
+            TextSurface = LoadedSurface
+        else:
+            TextSurface = FontObj.render(Text, False, Color, None)
+            SpriteLoadedText.append([Text+"&"+Font+"&"+str(Size),TextSurface])
+
+        if LoadedSurfaceShadow != False:
+            TextSurface = LoadedSurface
+        else:
+            LoadedSurfaceShadow = FontObj.render(Text, False, (0,0,0), None)
+            SpriteLoadedText.append([Text+"&"+Font+"&"+str(Size)+"&shadow",LoadedSurfaceShadow])
+            
+        Surface.blit(LoadedSurfaceShadow,(POS[0]-5,POS[1]-5))
+        Surface.blit(TextSurface,POS)
+        
+
+##class basic_button():
+##    def basic_button( surface, size , text, function, pos ):
+##    text_object( surface, size, text, "Warlords.ttf", (255,255,255), (pos[0],pos[1]) )
+    
